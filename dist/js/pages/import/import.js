261eb105393a6083522f6b40a698913f4f395035
kuroko84 <ttphong071016@gmail.com>
2023-06-03 03:30:24 +0700

    tach trang nhap hang, kho hang

--- dist/js/pages/import/import.js
+++ dist/js/pages/import/import.js
@@ -1,7 +1,7 @@
-//item list table setting
+//import item table setting
 $(document).ready(function () {
   // Kích hoạt DataTables
-  $("#ItemWarehouseTable").DataTable({
+  $("#ImportItemWarehouseTable").DataTable({
     // Cấu hình thanh tìm kiếm
     searching: true,
     // Cấu hình điều hướng trang
@@ -24,10 +24,45 @@ $(document).ready(function () {
     },
   });
 });
-//item table
+
+//import item table setting
 $(document).ready(function () {
   // Kích hoạt DataTables
-  $("#ItemTable").DataTable({
+  $("#ImportItemHistoryWarehouseTable").DataTable({
+    // Cấu hình thanh tìm kiếm
+    searching: true,
+    // Cấu hình điều hướng trang
+    paging: true,
+    // Cấu hình số bản ghi hiển thị trên mỗi trang
+    pageLength: 10,
+    // Cấu hình ngôn ngữ hiển thị
+    language: {
+      search: "Tìm kiếm:",
+      lengthMenu: "Hiển thị _MENU_ bản ghi",
+      info: "Hiển thị từ _START_ đến _END_ của _TOTAL_ bản ghi",
+      infoEmpty: "Hiển thị từ 0 đến 0 của 0 bản ghi",
+      infoFiltered: "(được lọc từ tổng số _MAX_ bản ghi)",
+      paginate: {
+        first: "Đầu",
+        last: "Cuối",
+        next: "Tiếp",
+        previous: "Trước",
+      },
+    },
+  });
+});
+
+//pop up nhập hàng
+document
+  .getElementById("HoanThanhThanhToan")
+  .addEventListener("click", function () {
+    // Tìm popup theo id và ẩn nó
+  });
+
+//popup chi tiết nhập
+$(document).ready(function () {
+  // Kích hoạt DataTables
+  $("#HistoryDetailWarehouseTable").DataTable({
     // Cấu hình thanh tìm kiếm
     searching: true,
     // Cấu hình điều hướng trang
