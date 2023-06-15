bb6a4f59f8a7dfbbc1f6cb4b552bcfc41c263df3
kuroko84 <ttphong071016@gmail.com>
2023-06-15 16:00:33 +0700

    option page

---
+++ dist/js/pages/option/option.js
@@ -0,0 +1,51 @@
+// table supplier
+$(document).ready(function () {
+  // Kích hoạt DataTables
+  $("#CarBrandTable").DataTable({
+    // Cấu hình thanh tìm kiếm
+    searching: true,
+    // Cấu hình điều hướng trang
+    paging: true,
+    // Cấu hình số bản ghi hiển thị trên mỗi trang
+    pageLength: 10,
+    // Cấu hình ngôn ngữ hiển thị
+    language: {
+      search: "Tìm kiếm:",
+      lengthMenu: "Hiển thị _MENU_ bản ghi",
+      info: "Hiển thị từ _START_ đến _END_ của _TOTAL_ bản ghi",
+      infoEmpty: "Hiển thị từ 0 đến 0 của 0 bản ghi",
+      infoFiltered: "(được lọc từ tổng số _MAX_ bản ghi)",
+      paginate: {
+        first: "Đầu",
+        last: "Cuối",
+        next: "Tiếp",
+        previous: "Trước",
+      },
+    },
+  });
+});
+$(document).ready(function () {
+  // Kích hoạt DataTables
+  $("#WageListTable").DataTable({
+    // Cấu hình thanh tìm kiếm
+    searching: true,
+    // Cấu hình điều hướng trang
+    paging: true,
+    // Cấu hình số bản ghi hiển thị trên mỗi trang
+    pageLength: 10,
+    // Cấu hình ngôn ngữ hiển thị
+    language: {
+      search: "Tìm kiếm:",
+      lengthMenu: "Hiển thị _MENU_ bản ghi",
+      info: "Hiển thị từ _START_ đến _END_ của _TOTAL_ bản ghi",
+      infoEmpty: "Hiển thị từ 0 đến 0 của 0 bản ghi",
+      infoFiltered: "(được lọc từ tổng số _MAX_ bản ghi)",
+      paginate: {
+        first: "Đầu",
+        last: "Cuối",
+        next: "Tiếp",
+        previous: "Trước",
+      },
+    },
+  });
+});
